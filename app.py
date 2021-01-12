ee4ca2864e19365b90f2a007d37cd3ea651a1700
tkdwltjd <75294615+tkdwltjd@users.noreply.github.com>
2021-01-12 22:19:23 +0900

    질문페이지 형식 만들기
    질문 페이지 형식을 만들었다.

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/test')
+def get_test_page():
+    return render_template('question1.html')
 
 
 if __name__ == '__main__':
