a29e9b082a79d56969f3ed91143fce0455eedcb5
tkdwltjd <75294615+tkdwltjd@users.noreply.github.com>
2021-01-14 22:33:31 +0900

    qna추가

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,18 @@ app = Flask(__name__)
 def get_test_page():
     return render_template('question1.html')
 
+@app.route('/')
+def get_main_page():
+    return render_template('index.html')
+
+@app.route('/question')
+def get_question_page():
+    return render_template('question1.html')
+
+@app.route('/question', methods=['GET'])
+def show_qna():
+    return jsonify({'result': 'success'})
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run('0.0.0.0',debug=True,port=5000)
