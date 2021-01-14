89d3327948923647dc7120a6ca23a42712a8cf2b
Joo, Jeong Han <joo3436@gmail.com>
2021-01-14 22:55:59 +0900

    해결

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
 
+client = MongoClient('localhost', 27017)
+db = client.my_tumblr
 app = Flask(__name__)
 
 
@@ -15,10 +18,21 @@ def get_main_page():
 def get_question_page():
     return render_template('question1.html')
 
-@app.route('/question', methods=['GET'])
+@app.route('/resut')
+def get_result_page():
+    answers = request.args.get('answers')
+
+    #
+
+    return render_template('result.html')
+
+
+
+@app.route('/qnas', methods=['GET'])
 def show_qna():
-    return jsonify({'result': 'success'})
+    qnas = list(db.qna.find({}, {'_id': False}))
+    return jsonify({'result': 'success', 'qna': qnas})
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',debug=True,port=5000)
+    app.run('0.0.0.0', debug=True, port=5000)
