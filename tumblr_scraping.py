5e0a52c2c975315106f14a25523b75889bcb6956
tkdwltjd <75294615+tkdwltjd@users.noreply.github.com>
2021-01-18 21:42:42 +0900

    (질문)셀레니움 크롤링

--- tumblr_scraping.py
+++ tumblr_scraping.py
@@ -1,20 +1,11 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
 
-# 타겟 URL을 읽어서 HTML를 받아오고,
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('http://search.danawa.com/dsearch.php?k1=%ED%85%80%EB%B8%94%EB%9F%AC&module=goods&act=dispMain', headers=headers)
+driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+driver.implicitly_wait(2)
+driver.get('http://search.danawa.com/dsearch.php?k1=%ED%85%80%EB%B8%94%EB%9F%AC&module=goods&act=dispMain')
 
-# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
-# soup이라는 변수에 "파싱 용이해진 html"이 담긴 상태가 됨
-# 이제 코딩을 통해 필요한 부분을 추출하면 된다.
-soup = BeautifulSoup(data.text, 'html.parser')
-
-#############################
-# (입맛에 맞게 코딩)
-#############################
+locate = '#productItem8006071 > div > div.prod_info > p > a'
+element = driver.find_element_by_css_selector(locate)
+print(element.text.split)
 
 
-tumblrs = soup.select('#productListArea > div.main_prodlist.main_prodlist_list > ul > li')
-
-print(tumblrs)
