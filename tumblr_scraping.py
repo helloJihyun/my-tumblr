10b7291582c2f1a022ec88542b28055f24f3a675
helloJihyun <75294615+helloJihyun@users.noreply.github.com>
2021-01-18 21:44:25 +0900

    Update tumblr_scraping.py

--- tumblr_scraping.py
+++ tumblr_scraping.py
@@ -6,6 +6,6 @@ driver.get('http://search.danawa.com/dsearch.php?k1=%ED%85%80%EB%B8%94%EB%9F%AC&
 
 locate = '#productItem8006071 > div > div.prod_info > p > a'
 element = driver.find_element_by_css_selector(locate)
-print(element.text.split)
+print(element.text.split('\n'))
 
 
