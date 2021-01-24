51920de22602d3fd3c0c88ff7005462dbb48860e
tkdwltjd <75294615+tkdwltjd@users.noreply.github.com>
2021-01-24 19:20:49 +0900

    result data 추가, reuslt 페이지 CSS 수정

--- data_qna.py
+++ data_qna.py
@@ -82,7 +82,7 @@ qna_data.append(
      })
     # 11번 Q&A
 qna_data.append(
-    {'q': '심각한 환경오염 영상을 본 후',
+    {'q': '심각한 환경오염 영상을 본 ',
      'a': [
          ('일회용품을 평소와 같이 사용한다.', {'type1' :0, 'type2' :0, 'type3' :0, 'type4' :0, 'type5' :0, 'type6' :0, 'type7' :3, 'type8' :0}),
          ('최대한 일회용품 사용을 줄이려고 노력한다.', {'type1' :0, 'type2' :0, 'type3' :0, 'type4' :0, 'type5' :0, 'type6' :0, 'type7' :0, 'type8' :0}),
@@ -99,11 +99,11 @@ qna_data.append(
 
 result_data = {}
 
-result_data['type1'] = ['한 우물만 파는 당신,','클래식 텀블러']
-result_data['type2'] = ['언제나 새로움을 추구하는 당신,','카멜레온 텀블러']
-result_data['type3'] = ['언제나 핫한 당신,','핫!뜨거 텀블러']
-result_data['type4'] = ['언제 어디서나 쿨한 당신,','앗!차거 텀블러']
-result_data['type5'] = ['오지에 가도 살아남을 당신,','all in one 텀블러']
-result_data['type6'] = ['의자와 물아일체된 당신,','망부석 텀블러']
-result_data['type7'] = ['환경을 두번 생각하는 당신,','친환경 텀블러']
-result_data['type8'] = ['도구를 쓸 줄 아는 당신,','꿀기능 텀블러']
+result_data['type1'] = ['한 우물만 파는 당신,','클래식 텀블러','한 우물만 팔 수 있다는 건 굉장히 대단한 능력이에요. 한번 시작한 일은 끝을 보고야 마는 당신은 뭐든지 이루어낼 수 있겠네요!','당신과 닮은 텀블러', '1913년부터 텀블러만 만들었다.','Stanley','스탠리','http://www.citycreek.co.kr/shopimages/20201110143655_509_0.jpg']
+result_data['type2'] = ['언제나 새로움을 추구하는 당신,','카멜레온 텀블러','당신의 변화무쌍한 모습은 항상 주변인에게 일상의 영감이 됩니다. 새로운 도전도 마다하지 않는 당신은 이 시대에 꼭 필요한 사람이네요!','당신과 닮은 텀블러','기분에 따라 바꾸는 텀블러 색깔!','Alex','알렉스','https://www.alexlife.co.kr/wp-content/uploads/2018/06/alex-tumbler-16oz-white-lilac-copy.jpg']
+result_data['type3'] = ['언제나 핫한 당신,','핫!뜨거 텀블러','꽁꽁 얼어버린 마음도 한순간에 녹일 만큼 따뜻한 감성을 가지셨네요. 세상을 당신의 편으로 만들어버리는 대단한 능력!','당신과 닮은 텀블러','독보적 진공 단열 기술로 따뜻하게','THERMOS','써모스','https://thermosshop.kr/web/product/extra/big/20200602/46c1c4c88597ac63a80e9f6da1209f3d.png']
+result_data['type4'] = ['언제 어디서나 쿨한 당신,','앗!차거 텀블러','당신의 쿨워터향에 모두가 이미 취해버렸어요. 현실적인 안목으로 무슨 일이든 당당하게 척척 해내는 당신은 만능 재주꾼!','당신과 닮은 텀블러','보기만 해도 시원해지는 텀블러','Pilmoa','필모아','http://pilmoa.com/web/product/medium/202012/acdcbc510c0763069a49242b5a9007f2.png']
+result_data['type5'] = ['오지에 가도 살아남을 당신,','아웃도어 텀블러','넘치는 에너지를 가진 도시의 모험가이시군요! 모험심으로 위기상황을 극복하고 새로운 세상을 개척하는 이 시대의 콜롬버스!','당신과 닮은 텀블러','최초의 스테인레스 텀블러','Klean Kanteen','클린켄틴','http://img.gqkorea.co.kr/gq/2018/09/style_5ba4bed9aefb5.jpg']
+result_data['type6'] = ['의자와 물아일체된 당신,','인도어 텀블러','그 누구보다 몰입력이 뛰어나군요! 한 번 몰입하면 진심을 다하는 당신, 당신의 결과물은 감히 그 누구도 쉽게 상상하지 못할거에요.','당신과 닮은 텀블러','커피에 커피를, 커피 찌꺼기 텀블','Huskee','허스키','https://media1.popsugar-assets.com/files/thumbor/tGtiNiDQcpwvcJZ5LvO2K_Bp41s/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2019/12/09/197/n/36735815/68506a375d8f9984_Warm_1_1024x1024_2x/i/HuskeeCup-SmoothieJuiceIced-Coffee-Huskee-Cup-Straw-Bundle35.jpg']
+result_data['type7'] = ['환경을 두번 생각하는 당신,','친환경 텀블러','우러러 볼수록 높아만지는 환경을 향한 당신의 마음. 혼란스러운 세상에서도 본질적인 가치를 알고 있는 당신은 절대 무너지지 않는 단단한 사람입니다.','당신과 닮은 텀블러','판매의 10퍼센트를 환경단체에 기부',"S'well",'스웰','http://img.gqkorea.co.kr/gq/2018/09/style_5ba4bedd27ad5.jpg']
+result_data['type8'] = ['도구를 쓸 줄 아는 당신,','꿀기능 텀블러','도구를 사용하면 몸이 덜 고생한다! 그 누구보다 도구를 잘 사용하는 당신은 동시에 여러가지 일을 척척 해낼 수 있는 멀티플레이어이군요!','당신과 닮은 텀블러','물 마실 시간을 알려주는 스마트 텀블러','EQUA','EQUA','https://cdn.shopify.com/s/files/1/0252/8526/3469/products/equa-smart-water-bottle-pink_400x.gif?v=1605648714']
